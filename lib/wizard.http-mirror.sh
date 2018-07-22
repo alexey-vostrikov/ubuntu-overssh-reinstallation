e8d4b0b6b82388d622d172be8b75d4dbdc0d0d56
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2018-07-23 01:24:57 +0430

    change for fix case sensive also home folder path

--- lib/wizard.http-mirror.sh
+++ lib/wizard.http-mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEFAULT_MIRROR_HTTP_PROXY=$(apt-config dump | grep "Acquire::HTTP::Proxy" | cut -d '"' -f2)
+DEFAULT_MIRROR_HTTP_PROXY=$(apt-config dump | grep -i "Acquire::HTTP::Proxy" | cut -d '"' -f2)
 
 MIRROR_HTTP_PROXY=$(whiptail --title "$PROG_TITLE" --inputbox "Set mirror http proxy for apt:" $SIZE_X $SIZE_Y "$DEFAULT_MIRROR_HTTP_PROXY" 3>&1 1>&2 2>&3)
 
--- lib/wizard.publickey.sh
+++ lib/wizard.publickey.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+AUTHFILE="$HOME/.ssh/authorized_keys"
 DEFAUTL_PUBLICKEY=''
-if [ -e "~/.ssh/authorized_keys" ]; then
-  DEFAUTL_PUBLICKEY=$(head -n 1 ~/.ssh/authorized_keys)
+if [ -e "$AUTHFILE" ]; then
+  DEFAUTL_PUBLICKEY=$(head -n 1 $AUTHFILE)
 fi
 
 PUBLICKEY=$(whiptail --title "$PROG_TITLE" --inputbox "Set public key for authorized_keys:" $SIZE_X $SIZE_Y "$DEFAUTL_PUBLICKEY" 3>&1 1>&2 2>&3)
